2def90d33c12448628a3c8572ae0ca50584c67b0
Brian Ashburn <ashburn.brian@gmail.com>
2019-02-02 13:40:17 -0500

    Have one application expose Prometheus metrics

--- java-spring-boot/noun-service/src/main/java/com/bashburn/demo/nounservice/NounApplication.java
+++ java-spring-boot/noun-service/src/main/java/com/bashburn/demo/nounservice/NounApplication.java
@@ -1,20 +1,12 @@
 package com.bashburn.demo.nounservice;
 
 import org.springframework.boot.SpringApplication;
-import org.springframework.boot.actuate.autoconfigure.metrics.MeterRegistryCustomizer;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.context.annotation.Bean;
-
-import io.micrometer.core.instrument.MeterRegistry;
 
 @SpringBootApplication
 public class NounApplication {
   public static void main(String[] args) {
     SpringApplication.run(NounApplication.class, args);
-  }
-
-  @Bean
-  MeterRegistryCustomizer<MeterRegistry> metricsCommonTags() {
-    return registry -> registry.config().commonTags("application", "Elizabethan Insult");
   }
 }
