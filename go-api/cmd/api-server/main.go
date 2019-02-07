182663b5c91191782a72212b5fdc014f700674fe
Brian Ashburn <ashburn.brian@gmail.com>
2019-02-07 08:56:04 -0800

    Updating our go implementation to use Dep

--- go-api/cmd/api-server/main.go
+++ go-api/cmd/api-server/main.go
@@ -4,6 +4,7 @@ import (
 	"context"
 	"fmt"
 	"github.com/gorilla/mux"
+	"github.com/signalfx/golib/datapoint"
 	"github.com/signalfx/golib/sfxclient"
 	"github.com/unrolled/render"
 	"github.com/urfave/negroni"
@@ -15,18 +16,18 @@ func main() {
 	fmt.Println("Go API Example")
 	r := render.New()
 	m := mux.NewRouter()
-	sfx := sfxclient.NewHTTPDatapointSink()
-	token := os.Getenv("SFX_TOKEN")
+	sfx := sfxclient.NewHTTPSink()
+	sfx.AuthToken = os.Getenv("SFX_TOKEN")
 	ctx := context.Background()
 	dims := make(map[string]string)
-	dims['app'] = 'k8ws-golang-negroni'
+	dims["app"] = "k8ws-golang-negroni"
 
 	m.HandleFunc("/healthz", func(w http.ResponseWriter, req *http.Request) {
-		client.AddDatapoints(ctx, []*datapoint.Datapoint{
-			Gauge("k8ws.negroni.path.healthz.calls", dims, 1),
-			Cumulative("k8sws.negroni.path.healthz.calls_cumulative", dims, 1),
+		_ = sfx.AddDatapoints(ctx, []*datapoint.Datapoint{
+			sfxclient.Gauge("k8ws.negroni.path.healthz.calls", dims, 1),
+			sfxclient.Cumulative("k8sws.negroni.path.healthz.calls_cumulative", dims, 1),
 		})
-		r.Text(w, http.StatusOK, "OK")
+		_ = r.Text(w, http.StatusOK, "OK")
 	})
 
 	n := negroni.Classic()
